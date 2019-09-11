62225958d6b8b4659819e3a1ac01b52d315e2e6a
ScarWu <xneriscool@gmail.com>
2019-09-11 12:26:09 +0800

    1. update skill id

--- tools/syncSetSkill.php
+++ tools/syncSetSkill.php
@@ -4,7 +4,7 @@
 $root = __DIR__;
 
 // Composer Auto Loader
-include "{$root}/common.php";
+include "{$root}/crawler/common.php";
 
 $sets = [];
 $sets = array_merge($sets, loadJson("../../json/mhw/sets"));
@@ -28,23 +28,26 @@ foreach ($skills as $skill) {
     }
 }
 
+/**
+ * MHWIB
+ */
+// Armor
 for ($rare = 9; $rare <= 12; $rare++) {
     $bundles = loadJson("../../json/mhwib/armors/rare{$rare}");
 
     foreach ($bundles as $bundleIndex => $bundle) {
         if (false !== isset($bundle['common']['set'])) {
             if (false === isset($setNameMapping[$bundle['common']['set']['id']])) {
-                echo "Untrack Set: {$bundle['common']['set']['id']}\n";
+                echo "Armor Untrack Set: {$bundle['common']['set']['id']}\n";
             } else {
                 $bundle['common']['set']['id'] = $setNameMapping[$bundle['common']['set']['id']];
             }
         }
 
         foreach ($bundle['list'] as $armorIndex => $armor) {
-
             foreach ($armor['skills'] as $skillIndex => $skill) {
                 if (false === isset($skillNameMapping[$skill['id']])) {
-                    echo "Untrack Skill: {$skill['id']}\n";
+                    echo "Armor Untrack Skill: {$skill['id']}\n";
                 } else {
                     $armor['skills'][$skillIndex]['id'] = $skillNameMapping[$skill['id']];
                 }
@@ -56,5 +59,22 @@ for ($rare = 9; $rare <= 12; $rare++) {
         $bundles[$bundleIndex] = $bundle;
     }
 
-    saveJson("../../json/mhwib/armors_tmp/rare{$rare}", $bundles);
+    saveJson("../../json/mhwib/armors/rare{$rare}", $bundles);
 }
+
+// Jewel
+$jewels = loadJson("../../json/mhwib/jewels");
+
+foreach ($jewels as $chramIndex => $chram) {
+    foreach ($chram['skills'] as $skillIndex => $skill) {
+        if (false === isset($skillNameMapping[$skill['id']])) {
+            echo "Jewel Untrack Skill: {$skill['id']}\n";
+        } else {
+            $chram['skills'][$skillIndex]['id'] = $skillNameMapping[$skill['id']];
+        }
+    }
+
+    $jewels[$chramIndex] = $chram;
+}
+
+saveJson("../../json/mhwib/jewels", $jewels);
