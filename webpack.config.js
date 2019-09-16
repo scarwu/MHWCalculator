ee4eef280e5dc2d3c9fa9c85028dc7896e6e657e
ScarWu <xneriscool@gmail.com>
2019-09-16 14:39:30 +0800

    1. update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@
 
 const path = require('path');
 const marked = require("marked");
-const renderer = new marked.Renderer();
 
 module.exports = {
     mode: 'development',
@@ -38,42 +37,41 @@ module.exports = {
             {
                 test: /.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        query: {
-                            cacheDirectory: true,
-                            plugins: [
-                                // Stage 0
-                                "@babel/plugin-proposal-function-bind",
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        plugins: [
+                            // Stage 0
+                            "@babel/plugin-proposal-function-bind",
 
-                                // Stage 1
-                                "@babel/plugin-proposal-export-default-from",
-                                "@babel/plugin-proposal-logical-assignment-operators",
-                                ["@babel/plugin-proposal-optional-chaining", { "loose": false }],
-                                ["@babel/plugin-proposal-pipeline-operator", { "proposal": "minimal" }],
-                                ["@babel/plugin-proposal-nullish-coalescing-operator", { "loose": false }],
-                                "@babel/plugin-proposal-do-expressions",
+                            // Stage 1
+                            "@babel/plugin-proposal-export-default-from",
+                            "@babel/plugin-proposal-logical-assignment-operators",
+                            ["@babel/plugin-proposal-optional-chaining", { "loose": false }],
+                            ["@babel/plugin-proposal-pipeline-operator", { "proposal": "minimal" }],
+                            ["@babel/plugin-proposal-nullish-coalescing-operator", { "loose": false }],
+                            "@babel/plugin-proposal-do-expressions",
 
-                                // Stage 2
-                                ["@babel/plugin-proposal-decorators", { "legacy": true }],
-                                "@babel/plugin-proposal-function-sent",
-                                "@babel/plugin-proposal-export-namespace-from",
-                                "@babel/plugin-proposal-numeric-separator",
-                                "@babel/plugin-proposal-throw-expressions",
+                            // Stage 2
+                            ["@babel/plugin-proposal-decorators", { "legacy": true }],
+                            "@babel/plugin-proposal-function-sent",
+                            "@babel/plugin-proposal-export-namespace-from",
+                            "@babel/plugin-proposal-numeric-separator",
+                            "@babel/plugin-proposal-throw-expressions",
 
-                                // Stage 3
-                                "@babel/plugin-syntax-dynamic-import",
-                                "@babel/plugin-syntax-import-meta",
-                                ["@babel/plugin-proposal-class-properties", { "loose": false }],
-                                "@babel/plugin-proposal-json-strings"
-                            ],
-                            presets: [
-                                '@babel/preset-react'
-                            ]
-                        }
+                            // Stage 3
+                            "@babel/plugin-syntax-dynamic-import",
+                            "@babel/plugin-syntax-import-meta",
+                            ["@babel/plugin-proposal-class-properties", { "loose": false }],
+                            "@babel/plugin-proposal-json-strings"
+                        ],
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ]
                     }
-                ]
+                }
             },
             {
                 test: /\.md$/,
@@ -85,7 +83,7 @@ module.exports = {
                         loader: 'markdown-loader',
                         options: {
                             pedantic: true,
-                            renderer
+                            renderer: new marked.Renderer()
                         }
                     }
                 ]
